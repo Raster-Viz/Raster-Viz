0cffd3f55e1685ff428660aec6bbfe08d5ac6266
Calvin-Nowlen <64150047+Calvin-Nowlen@users.noreply.github.com>
2021-11-30 15:12:49 -0700

    Add files via upload

---
+++ raster-tools/docs/reference/index.rst
@@ -0,0 +1,14 @@
+.. currentmodule:: raster_tools
+
+#############
+API Reference
+#############
+
+.. toctree::
+   :maxdepth: 3
+
+   top_level
+   raster
+   vector
+   distance
+   focal
---
+++ raster-tools/setup.py
@@ -0,0 +1,35 @@
+from multiprocessing import cpu_count
+
+import numpy as np
+from Cython.Build import cythonize
+from Cython.Compiler import Options
+from setuptools import Extension, setup
+
+Options.fast_fail = True
+
+setup(
+    ext_modules=cythonize(
+        [
+            Extension(
+                "raster_tools.distance._heap",
+                ["raster_tools/distance/_heap.pyx"],
+                extra_compile_args=[
+                    "-O3",
+                    "-march=native",
+                    "-g0",
+                ],
+            ),
+            Extension(
+                "raster_tools.distance._core",
+                ["raster_tools/distance/_core.pyx"],
+                extra_compile_args=[
+                    "-O3",
+                    "-march=native",
+                    "-g0",
+                ],
+            ),
+        ],
+        nthreads=cpu_count(),
+    ),
+    include_dirs=[np.get_include()],
+)
