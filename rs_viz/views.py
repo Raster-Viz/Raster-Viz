eceb2ca17f6b8dc0373979f181d5c964c8331d98
BirdBrain2000 <nkitchel2000@outlook.com>
2022-02-23 09:03:08 -0700

    Removed return parameters so that is runs

--- rs_viz/views.py
+++ rs_viz/views.py
@@ -1,9 +1,5 @@
 import os
-import folium
 
-import numpy
-import xarray
-from django.core.exceptions import ValidationError
 from django.template import loader
 from django.http import HttpResponseRedirect, HttpResponse
 from django.shortcuts import render, redirect
@@ -42,120 +38,70 @@ class CreateFileUpload(CreateView):
       
 # This function creates the home page view for the web application
 def index(request):
-
-    # Creates the Map View's default folium map
-    m = folium.Map(location=[46.8721, -113.9940], control_scale ='True', zoom_start=14)
-    #test = folium.Html('<b>Hello world</b>', script=True)
-    #popup = folium.Popup(test, max_width=2650)
-    #folium.RegularPolygonMarker(location=[51.5, -0.25], popup=popup).add_to(m)
-    m = m._repr_html_() #updated
-
     layers = Layer.objects.filter(activated=True)
     vocal = None
     i = 0
-    raster = 0
+    rs = 0
+    flag ={'red_flag':False}
     for layer in layers:
-        rs = create_raster.create_raster(layer.document.path)
         if (i == 0):
-            raster = rs
+            rs = create_raster.create_raster(layer.document.path)
             i += 1
-            continue
-        arr = rs._to_presentable_xarray()
-        try:
-            raster = create_raster.add_to_raster(raster, rs)
-        except ValueError:
-            print(type(raster))
-            fs = raster._to_presentable_xarray()
-            fs.combine_first(arr)
-            raster = create_raster.add_to_raster(raster, fs)
+        else:
+            raster = create_raster.create_raster(layer.document.path)
+            try:
+                rs = create_raster.add_to_raster(rs, raster)
+            except ValueError:
+                vocal = "A value Error was raised in" + layer.name
+                layer.activated = False
+                layer.save()
+                flag={'red_flag':True}
 
-    context = {'folMap': m,
-                'vocal': vocal}
 
-    return render(request, 'rs_viz/index.html', context)
+    voc = {'vocal': vocal}
+    return render(request, 'rs_viz/index.html')
 
 # Create your views here.
 from pylab import figure, axes, pie, title
 from matplotlib.backends.backend_agg import FigureCanvasAgg
 
 def test_matplotlib(request):
-    try:
-        plt.clf()
-        layers = Layer.objects.filter(activated=True)
-        i = 0
-        raster = 0
-        for layer in layers:
+    plt.clf()
+    layers = Layer.objects.filter(activated=True)
+    i = 0
+    rs = 0
+    for layer in layers:
+        if (i == 0):
             rs = create_raster.create_raster(layer.document.path)
-            if (i == 0):
-                raster = rs
-                i += 1
-                continue
-            arr = rs._to_presentable_xarray()
-            try:
-                raster = create_raster.add_to_raster(raster, rs)
-            except ValueError:
-                print(type(raster))
-                fs = raster._to_presentable_xarray()
-                fs.combine_first(arr)
-                raster = create_raster.add_to_raster(raster, fs)
-        arr = raster._to_presentable_xarray()
-        if(arr.shape[0]!=3):
-            arr.plot()
+            i += 1
         else:
-            arr.plot.imshow()
-        f = plt.gcf()
-        canvas = FigureCanvasAgg(f)
-        response = HttpResponse(content_type='image/png')
-        canvas.print_png(response)
-        return response
-    except AttributeError:
-        return redirect('index')
-
+            raster = create_raster.create_raster(layer.document.path)
+            rs = create_raster.add_to_raster(rs, raster)
+    arr = rs._to_presentable_xarray()
+    arr.plot()
+    f = plt.gcf()
+    canvas = FigureCanvasAgg(f)
+    response = HttpResponse(content_type='image/png')
+    canvas.print_png(response)
+    return response
   
 class HelpPageView(TemplateView):
     template_name = 'rs_viz/help.html'
 
+
+
+
 def model_test(request):
     layers = Layer.objects.filter(activated = True)
     context = {"layers": layers}
-    vocal = None
     i = 0
-    raster = 0
-    List = {}
+    rs = 0
     for layer in layers:
-        rs = create_raster.create_raster(layer.document.path)
-        arr = rs._to_presentable_xarray()
-        print(arr.shape)
-        if arr.shape in List:
-            val = List.get(arr.shape)
-            val.append(layer.document)
-            List.update({arr.shape:val})
-        else:
-            List.update({arr.shape:[layer.document]})
-        if(i==0):
-            raster = rs
+        if(i == 0):
+            rs = create_raster.create_raster(layer.document.path)
             i+=1
-            continue
-        try:
-            raster = create_raster.add_to_raster(raster, rs)
-        except ValueError:
-            print(type(raster))
-            fs = raster._to_presentable_xarray()
-            fs.combine_first(arr)
-            raster = create_raster.add_to_raster(raster,fs)
-
-
-    context.update({'List':List})
+        else:
+            raster = create_raster.create_raster(layer.document.path)
+            rs = create_raster.add_to_raster(rs,raster)
+    rast = {"rast": rs}
     return render(request, 'rs_viz/fig.html', context)
-
-
-def show_map(request):
-
-    m = folium.Map(location=[46.8721, -113.9940], control_scale ='True', zoom_start=14)
-    #test = folium.Html('<b>Hello world</b>', script=True)
-    #popup = folium.Popup(test, max_width=2650)
-    #folium.RegularPolygonMarker(location=[51.5, -0.25], popup=popup).add_to(m)
-    m = m._repr_html_() #updated
-
-    context = {'my_map': m}
-    return render(request, 'rs_viz/index.html', context)
