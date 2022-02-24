f8b10615856d73eb40746f7050b9e3a80caf76c1
calvin <cal.nowlen@gmail.com>
2022-02-23 17:39:54 -0700

    merged code with calvin-dev

--- rs_viz/models.py
+++ rs_viz/models.py
@@ -8,11 +8,23 @@ from raster_tools import Raster
 # This model is a template for importing raster objects into
 # the web database, and provides a name for the layer. The activated
 # var sets up a potential method that may be used to turning layers 'on and off'
+def validate_file_extension(value):
+    import os
+    ext = os.path.splitext(value.name)[1]
+    valid_extensions = ['.tif', '.jpeg', '.png']
+    if not ext in valid_extensions:
+        raise ValidationError(u'File not supported!')
+
 
 class Layer(models.Model):
     name = models.CharField(max_length=100)
-    document = models.FileField(upload_to='rs_viz/')
+    document = models.FileField(upload_to='rs_viz/', validators=[validate_file_extension])
     activated = models.BooleanField(blank=True, default=True)
+
+    @property
+    def get_Raster(self):
+        return Raster(self.document.path)
+
     def __str__(self):
         return self.name
 
--- rs_viz/views.py
+++ rs_viz/views.py
@@ -1,6 +1,9 @@
 import os
 import folium
 
+import numpy
+import xarray
+from django.core.exceptions import ValidationError
 from django.template import loader
 from django.http import HttpResponseRedirect, HttpResponse
 from django.shortcuts import render, redirect
@@ -50,50 +53,64 @@ def index(request):
     layers = Layer.objects.filter(activated=True)
     vocal = None
     i = 0
-    rs = 0
-    flag ={'red_flag':False}
+    raster = 0
     for layer in layers:
+        rs = create_raster.create_raster(layer.document.path)
         if (i == 0):
-            rs = create_raster.create_raster(layer.document.path)
+            raster = rs
             i += 1
-        else:
-            raster = create_raster.create_raster(layer.document.path)
-            try:
-                rs = create_raster.add_to_raster(rs, raster)
-            except ValueError:
-                vocal = "A value Error was raised in" + layer.name
-                layer.activated = False
-                layer.save()
-                flag={'red_flag':True}
+            continue
+        arr = rs._to_presentable_xarray()
+        try:
+            raster = create_raster.add_to_raster(raster, rs)
+        except ValueError:
+            print(type(raster))
+            fs = raster._to_presentable_xarray()
+            fs.combine_first(arr)
+            raster = create_raster.add_to_raster(raster, fs)
 
     context = {'folMap': m,
                 'vocal': vocal}
 
-    return render(request, 'rs_viz/index.html', context, flag)
+    return render(request, 'rs_viz/index.html', context)
 
 # Create your views here.
 from pylab import figure, axes, pie, title
 from matplotlib.backends.backend_agg import FigureCanvasAgg
 
 def test_matplotlib(request):
-    plt.clf()
-    layers = Layer.objects.filter(activated=True)
-    i = 0
-    rs = 0
-    for layer in layers:
-        if (i == 0):
+    try:
+        plt.clf()
+        layers = Layer.objects.filter(activated=True)
+        i = 0
+        raster = 0
+        for layer in layers:
             rs = create_raster.create_raster(layer.document.path)
-            i += 1
+            if (i == 0):
+                raster = rs
+                i += 1
+                continue
+            arr = rs._to_presentable_xarray()
+            try:
+                raster = create_raster.add_to_raster(raster, rs)
+            except ValueError:
+                print(type(raster))
+                fs = raster._to_presentable_xarray()
+                fs.combine_first(arr)
+                raster = create_raster.add_to_raster(raster, fs)
+        arr = raster._to_presentable_xarray()
+        if(arr.shape[0]!=3):
+            arr.plot()
         else:
-            raster = create_raster.create_raster(layer.document.path)
-            rs = create_raster.add_to_raster(rs, raster)
-    arr = rs._to_presentable_xarray()
-    arr.plot()
-    f = plt.gcf()
-    canvas = FigureCanvasAgg(f)
-    response = HttpResponse(content_type='image/png')
-    canvas.print_png(response)
-    return response
+            arr.plot.imshow()
+        f = plt.gcf()
+        canvas = FigureCanvasAgg(f)
+        response = HttpResponse(content_type='image/png')
+        canvas.print_png(response)
+        return response
+    except AttributeError:
+        return redirect('index')
+
   
 class HelpPageView(TemplateView):
     template_name = 'rs_viz/help.html'
@@ -101,16 +118,34 @@ class HelpPageView(TemplateView):
 def model_test(request):
     layers = Layer.objects.filter(activated = True)
     context = {"layers": layers}
+    vocal = None
     i = 0
-    rs = 0
+    raster = 0
+    List = {}
     for layer in layers:
-        if(i == 0):
-            rs = create_raster.create_raster(layer.document.path)
-            i+=1
+        rs = create_raster.create_raster(layer.document.path)
+        arr = rs._to_presentable_xarray()
+        print(arr.shape)
+        if arr.shape in List:
+            val = List.get(arr.shape)
+            val.append(layer.document)
+            List.update({arr.shape:val})
         else:
-            raster = create_raster.create_raster(layer.document.path)
-            rs = create_raster.add_to_raster(rs,raster)
-    rast = {"rast": rs}
+            List.update({arr.shape:[layer.document]})
+        if(i==0):
+            raster = rs
+            i+=1
+            continue
+        try:
+            raster = create_raster.add_to_raster(raster, rs)
+        except ValueError:
+            print(type(raster))
+            fs = raster._to_presentable_xarray()
+            fs.combine_first(arr)
+            raster = create_raster.add_to_raster(raster,fs)
+
+
+    context.update({'List':List})
     return render(request, 'rs_viz/fig.html', context)
 
 
