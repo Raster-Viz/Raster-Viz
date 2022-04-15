d17311a932eace423c43a48a2caf7fe4eb0f73ca
J.D. Holwick <70964794+jdholwick@users.noreply.github.com>
2022-04-15 14:13:02 -0600

    * Main merged into my changes that encompass drag-and-drop of the ToC.

--- rs_viz/static/rs_viz/script.js
+++ rs_viz/static/rs_viz/script.js
@@ -14,6 +14,60 @@ var btn_properties = document.getElementById("btn_properties");
 // Get the <span> element that closes the modal
 var spans = document.getElementsByClassName("close");
 
+// JD's drag-and-drop work:
+
+//var toc_layer = document.getElementById("active_layer1"); // TESTING: For now we'll start with just this one layer but we'll need to figure out how to grab them dynamically since the names are made on the fly.
+var draggable_layers = document.querySelectorAll('.draggable-layer'); // "Draggable-layer" is the class used in the HTML of the table row <tr> elements in the ToC (Table of Contents).
+var layer_container = document.querySelectorAll('.layer'); // The class "layer" is the container in the html.
+
+draggable_layers.forEach(draggable => {
+    draggable.addEventListener('dragstart', () => {
+        draggable.classList.add('dragging-layer')
+        //console.log('drag test');
+    });
+
+    draggable.addEventListener('dragend', () => {
+        draggable.classList.remove('dragging-layer')
+    });
+});
+
+layer_container.forEach(container => {
+    container.addEventListener('dragover', e => {
+        e.preventDefault();
+        const after_layer_elem = getDragAfterElement(container, e.clientY);
+        console.log(after_layer_elem);
+        const draggable = document.querySelector('.dragging-layer');
+        if (after_layer_elem == null) {
+            container.appendChild(draggable); // '.appendChild' is used to place the <tr> to the bottom of the rows it seems.
+        } else {
+            container.insertBefore(draggable, after_layer_elem);
+        }
+        //console.log('drag over test');
+    });
+});
+
+function getDragAfterElement(container, y) {
+    const draggable_layer_elem = [...container.querySelectorAll('.draggable-layer:not(.dragging-layer)')]; // The '[... ]' turn the '.querySelectorAll' into an array of layers and the 'not' excludes the one being drug.
+
+    return draggable_layer_elem.reduce((closest, child) => {
+        const box = child.getBoundingClientRect();
+        const offset = y - box.top - box.height / 2; // This takes the top of the 'box' which is a row essentially, and calculates where to drop the dragged row.
+        //console.log(box);
+        if (offset < 0 && offset > closest.offset) {
+            return { offset: offset, element: child};
+        } else {
+            return closest;
+        }
+    }, {offset: Number.NEGATIVE_INFINITY}).element;
+}
+
+/*toc_layer.ondragstart = function() {
+    // TESTING: We have shown this will print something to the screen so we know it's reactive on drag even though it does nothing right now.
+}*/
+
+//////////
+
+
 // When the user clicks the button, show correct modal
 btn_import.onclick = function() {
     modal.style.display = "block";
@@ -85,3 +139,20 @@ function toggle_visibility(id, separate) {
     e.style.display = 'block';
     v.style.display = 'none';
 }
+
+// Below JD is experimenting with draggable checkboxes
+
+//$tr = $("#trLayer > tr:hover");
+
+/*$(document).ready(function() {
+    addDragAttr();
+    //$tr.on("dragstart", layerCheckboxDragEvntHndlr);
+});
+
+function addDragAttr() {
+    $("#trLayer > td", #trLayer2 > td"").attr("draggable", "true"); // For whatever reason this code refused to work.
+}*/
+
+/*function layerCheckboxDragEvntHndlr(event) {
+    (event.originalEvent || event).dataTransfer.setData("text", $(this).text());
+}*/
